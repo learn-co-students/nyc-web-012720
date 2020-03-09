407c859ea8aa07fcdebdf4505ad3948ee1c73dd4
Steven Doran <steven.doran@flatironschool.com>
2020-03-09 10:20:00 -0400

    pre lecture

---
+++ 19-intro-to-js/index.js
@@ -0,0 +1,18 @@
+console.log('Hi there')
+
+
+// How to write functions in JavaScript
+
+  // write a function that prints 'hello world' to the console
+
+  // write a function that accepts a 'name' parameter and prints "Hi, my name is <name>" to the console
+
+  // write a funtion that accepts 2 numbers as parameters and adds them
+
+  // write a function that accepts a number as a parameter and doubles it
+
+  // write a function that accepts a number as a parameter and prints "❌ You're too young to enter the clurb! ❌" is the parameter 
+  // is less than 21 and "🤡 Welcome to the clurb! 🚀" if the parameter is 21 or over
+
+  // modify the above function to allow an underage patron in if they have a fake id
+
